c0abfaa98944374b3120ff22fa6283af0a0fe335
UnsuitableConfidence <161158334+UnsuitableConfidence@users.noreply.github.com>
2025-02-10 17:32:57 +0000

    Updated notes - no change to functionality

--- js/index.js
+++ js/index.js
@@ -49,12 +49,20 @@ function updateCity(event) {
 let citySelect = document.querySelector("#city");
 citySelect.addEventListener("change", updateCity);
 
+//Steps:
+//1. Adjust the time so updates every second
+//2. Show current location on LOAD and add home screen button
+//3. Remove weather emoji, edit CSS as needed
+//SUBMIT
+//4. Add up to 2 locations, then start deleting
+//5. Update for weather API for emoji
+//6. Add Advanced Dropdown
+//7. Update to 3 locations
+
+//Notes:
 //This can probably be improved using loops and injecting new values in each variable, with a max of 3)
 //weather emoji will use SheCodes weather API
 
 //Use an array to get the city list; Basically I want the array to be added to my js, when change is detected, search the array for the right "value" and spit out the tz value
 // i.e. click "London, UK" -> html value is "Europe/London" to be used by moment
 //Will need to figure out how this works with the API as well but that is a wholeeeee other project
-//I also like the idea of having the GMT value, if that's possible
-
-//https://www.w3schools.com/howto/howto_js_cascading_dropdown.asp Cascading drop down - "Select Region", then "Select City"
